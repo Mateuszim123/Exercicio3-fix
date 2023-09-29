f0ddf58a96c47810e0088d135fd08499a0f9ea8d
Mateuszim123 <57240384+Mateuszim123@users.noreply.github.com>
2023-09-29 11:37:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Exercicio3_fix;
+using System;
+
+namespace Exercicio3Fix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+
+            Console.Write("Nota 1:");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota 2:");
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota 3:");
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+        
+            if (aluno.NotaFinal() >= 60)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!!");
+                double falta = aluno.NotaFinal() - 60;
+                Console.WriteLine($"Faltaram {falta}");
+            }
+        }
+    }
+}
